4f3df2044a8b977256980fd6ac7f3befb1879de6
tsien16 <tsien16@qq.com>
2019-02-24 14:26:14 +0800

    ignore fixed

---
+++ src/main/java/com/tsien/mall/controller/backend/ProductManageController.java
@@ -0,0 +1,12 @@
+package com.tsien.mall.controller.backend;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author tsien
+ * @version 1.0.0
+ * @date 2019/2/19 0019 18:33
+ */
+
+public class ProductManageController {
+}
